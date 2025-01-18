242408b4bf710fd2d8a4a9ca5de4d53254e2a644
Wildan Ardiansyah <wild4nard@gmail.com>
2025-01-19 02:14:55 +0700

    fix

--- app/search/[keyword]/page.tsx
+++ app/search/[keyword]/page.tsx
@@ -1,20 +1,27 @@
 
-const Page  = async ({params}: {params: {keyword: string}}) => {
+import { NextPage } from 'next';
+import { ParsedUrlQuery } from 'querystring';
+
+interface Params extends ParsedUrlQuery {
+  keyword: string;
+}
+
+const Page: NextPage<{ params: Params }> = async ({ params }) => {
   const { keyword } = await params;
   const response = await fetch(`https://wartsanimeapi.vercel.app/api/search?s=${keyword}`);
   const searchAnime = await response.json();
   return (
-      <main className="text-white mx-auto w-full mb-auto mt-8">
-        <div className=" mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-          <div className="flex justify-between items-center mb-5">
-            <h1 className="text-2xl text-white font-semibold border-l-4 border-l-[#E94560] uppercase pl-2 pt-1">{keyword}</h1>
-          </div>
-          <div className="grid grid-cols-4 gap-4">
-            {
-              searchAnime.map((item: any) => (
-                <div className="product__item" key={item.linkid}>
-                  <div className="product__item__pic set-bg" style={{ backgroundImage: `url(${item.image})` }}>
-                  <div className={`status ${item.status === "Ongoing" ? "bg-green-900" : "bg-red-600"  }`}>
+    <main className="text-white mx-auto w-full mb-auto mt-8">
+      <div className=" mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between items-center mb-5">
+          <h1 className="text-2xl text-white font-semibold border-l-4 border-l-[#E94560] uppercase pl-2 pt-1">{decodeURIComponent(keyword)}</h1>
+        </div>
+        <div className="grid grid-cols-4 gap-4">
+          {
+            searchAnime.map((item: any) => (
+              <div className="product__item" key={item.linkid}>
+                <div className="product__item__pic set-bg" style={{ backgroundImage: `url(${item.image})` }}>
+                  <div className={`status ${item.status === "Ongoing" ? "bg-green-900" : "bg-red-600"}`}>
                     {item.status}
                   </div>
                   <div className="score">
@@ -25,23 +32,23 @@ const Page  = async ({params}: {params: {keyword: string}}) => {
                     <i className="fa fa-eye"></i>
                     <span>{item.view}</span>
                   </div>
-                  </div>
-                  <div className="product__item__text">
-                    <ul>
-                     {
+                </div>
+                <div className="product__item__text">
+                  <ul>
+                    {
                       item.genre.map((genre: any, index: number) => (
                         <li key={index}>{genre}</li>
                       ))
-                     }
-                    </ul>
-                    <h5 className="my-auto"><a href="#">{item.title}</a></h5>
-                  </div>
+                    }
+                  </ul>
+                  <h5 className="my-auto"><a href="#">{item.title}</a></h5>
                 </div>
-              ))
-            }
-          </div>
+              </div>
+            ))
+          }
         </div>
-      </main>
+      </div>
+    </main>
   )
 }
 
