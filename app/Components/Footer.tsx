173f1bb4931f79172aefddc264e6de05a6744b6b
Wildan Ardiansyah <wild4nard@gmail.com>
2025-01-18 22:12:29 +0700

    fe-fix

--- app/Components/Footer.tsx
+++ app/Components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <footer suppressHydrationWarning className="bg-base-200 text-base-content p-5">
+    <footer suppressHydrationWarning className="bg-[#070720] text-base-content p-5">
       <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
         <div className='flex justify-center items-center'>
           <p className='text-xs'>© Copyright 2023 Wartchnime. All Rights Reserved.</p>
--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ const Home = () => {
       <main className="text-white mx-auto mb-auto mt-8">
         <div className="mx-3 max-w-7xl px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center mb-5">
-            <h1 className="text-2xl text-white font-semibold border-l-4 border-l-orange-500 pl-2 pt-1">LATEST ANIME</h1>
+            <h1 className="text-2xl text-white font-semibold border-l-4 border-l-[#E94560] pl-2 pt-1">LATEST ANIME</h1>
             <h1 className="text-xs tracking-[.50em]">VIEW ALL →</h1>
           </div>
           <DataAnime />
